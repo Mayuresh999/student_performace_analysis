4b4da42b5d088fca2a40fe639bf2a71fa1b1c96a
unknown <mayuresh.madiwale@yahoo.com>
2024-12-26 12:34:15 +0530

    setting up project

--- setup.py
+++ setup.py
@@ -1,7 +1,20 @@
 import setuptools
+from typing import List
 
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
+
+HYPHEN_E_DOT = "-e ."
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    with open(file_path, 'r') as f:
+        requirements = f.readlines()
+        requirements=[req.replace("\n", "") for req in requirements]
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+    return requirements
+
 
 __version__ = '0.0.0'
 
@@ -23,4 +36,5 @@ setuptools.setup(
     project_urls= {"Student_Performace_Analyser" : f"https://github.com/{AUTHOR_USERNAME}/{REPO_NAME}/issues"},
     package_dir= {"" : "src"},
     packages=setuptools.find_packages(where="src"),
+    install_requires = get_requirements('requirements.txt')
 )
